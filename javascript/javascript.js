89d98dd7907b7ca2decb8d35e64dd16eaea1b8ba
Billy <billyjag1@gmail.com>
2023-04-13 14:30:58 -0400

    avance2

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,22 @@
 
-      function calcularIva() {
+function calcularIva() {
+
+    var porcentaje = document.getElementById("porcentaje").value;
+    if (porcentaje == 0) {
         var precio = document.getElementById("precio").value;
         var porcentaje = document.getElementById("porcentaje").value;
-        var iva = (precio * porcentaje) / 100;
+        var iva = (precio * 19) / 100;
         var precioConIva = parseFloat(precio) + parseFloat(iva);
         document.getElementById("resultado").innerHTML = precioConIva.toFixed(2);
-      }
-   
+        document.getElementById("resultadoIVA").innerHTML = iva.toFixed(2);
+    } else {
+        var precio = document.getElementById("precio").value;
+        var porcentaje = document.getElementById("porcentaje").value;
+        var iva = (precio * pocentaje) / 100;
+        var precioConIva = parseFloat(precio) + parseFloat(iva);
+        document.getElementById("resultado").innerHTML = precioConIva.toFixed(2);
+        document.getElementById("resultadoIVA").innerHTML = iva.toFixed(2);
+    }
+
+
+}
